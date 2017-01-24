0c08bcee8dbb1372a8d9de6226b50c01187b33cb
someonehappyau <someonehappyau@gmail.com>
2017-01-24 20:36:53 +1100

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var express=require('express');
 var app=express();
 
 app.get('/', (req, res)=>{
-    res.status(200).end('Hello CI world.\nIt\'s a nice day!\nHey!\n\bIt\' automatically built and updated by Jenkins and Docker!\n\nFantastic!');
+    res.status(200).end('Hello CI world.\nIt\'s a nice day!\nHey!\n\nIt\' automatically built and updated by Jenkins and Docker!\n\nFantastic!');
 });
 
 app.listen('3002');
