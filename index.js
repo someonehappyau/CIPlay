ae3406ff6ee3997770b717000f5c51af07a48046
jiadai <jiadai@nbnco.com.au>
2017-01-25 15:01:19 +1100

    use mysql

--- index.js
+++ index.js
@@ -66,5 +66,11 @@ app.get('/records', function(req, res){
 });
 
 app.use(express.static(path.join(__dirname,'public')));
+
+app.use(function(err, req, res, next){
+    console.error(err.stack);
+    res.status(500).end('Somthing goes wrong.');
+});
+
 app.listen('3002');
 console.log('listening at port 3002.');
