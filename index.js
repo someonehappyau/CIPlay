971dbb6192e0b8254cc4515ae92ffc46ef91377d
jiadai <jiadai@nbnco.com.au>
2017-01-25 14:22:24 +1100

    use mysql

--- index.js
+++ index.js
@@ -8,6 +8,15 @@ var fs=require('fs');
 var moment=require('moment');
 var shell=require('shelljs');
 
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+    host     : 'mysql',
+    user     : 'usertest',
+    password : 'iamtester',
+    database : 'test'
+});
+
+
 var DATA_PATH=path.join(__dirname,'data/records');
 
 if (fs.existsSync(DATA_PATH)===false){
@@ -22,8 +31,38 @@ app.post('/record', function(req, res){
     var now=Date.now();
     var fileName=moment().format('YYYYMMDDHHmmsszzzSSS')+'.txt';
     fs.writeFileSync(path.join(DATA_PATH, fileName), req.body.info);
-    res.status(200).end();
+
+    connection.connect();
+
+    connection.query('insert into ?? set ?',['record',{content:req.body.info}], function(err, result){
+        if (err){
+            res.status(500).end(err);
+        }
+        else{
+            res.redirect('/');
+        }
+        connection.close();
+    });
+
 });
+
+app.get('/records', function(req, res){
+    connection.connect();
+    connection.query('select * from record', function(err, result){
+        if (err){
+            res.status(500).end();
+        }
+        else{
+            var html='<ul>';
+            result.forEach(function(data){
+                html+='<li>'+data.id+'\t'+data.content+'\t'+moment(data.date_created).format('YYYY-MM-DD HH:mm:ss')+'</li>';
+            });
+            html+='</ul>';
+            res.status(200).end(html);
+        }
+        connection.close();
+    })
+})
 
 app.use(express.static(path.join(__dirname,'public')));
 app.listen('3002');
